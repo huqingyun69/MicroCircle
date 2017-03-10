477fe2c69f4f737d02b05d8793daf29a368ec48e
huqingyun69 <huqingyun69@qq.com>
2017-03-10 23:07:00 +0800

    the first commit all file

---
+++ src/main/java/cn/springmvc/controller/MainController.java
@@ -0,0 +1,18 @@
+package cn.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+	@RequestMapping("index")
+	public String index(String name ){
+		System.out.println(name);
+		return "index";
+	}
+	
+}
